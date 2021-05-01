50c8c1fa5433845f64b0e53e6bd579245f07d54e
sagar098 <sagar.nayak098@outlook.com>
2021-05-01 21:25:40 +0530

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using Newtonsoft.Json;
+using System.Net.Http;
+using System.Threading;
+using System.Configuration;
+
+namespace ConsoleApp5
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            string JsonData = await GetCowinData();
+         
+            Root allData = JsonConvert.DeserializeObject<Root>(JsonData);
+            var result = allData.centers.Where(a => a.sessions.Contains(a.sessions.Find(p => p.min_age_limit == 18 && p.available_capacity >= 0)));
+            if (!result.Any())
+            {
+                Console.WriteLine("No vaccine for 18+");
+            }
+            foreach (var info in result)
+            {
+                Console.WriteLine(string.Format("Hospital name : {0}, Pin : {1}", info.name, info.pincode));
+                foreach (var session in info.sessions)
+                {
+                    Console.WriteLine(string.Format("Session capacity : {0}, date : {1}, Vaccine : {2}, Age : {3}", Convert.ToString(session.available_capacity), session.date, session.vaccine, Convert.ToString(session.min_age_limit)));
+                }
+            }
+            
+            Console.ReadLine();
+
+        }
+
+        public static async Task<String> GetCowinData()
+        {
+            
+            string date = DateTime.Now.ToString("dd-MM-yyyy");
+            HttpClient client = new HttpClient();
+            string districtCode = ConfigurationSettings.AppSettings.Get("DistrictCode");
+            HttpResponseMessage response = await client.GetAsync(String.Format("https://cdn-api.co-vin.in/api/v2/appointment/sessions/public/calendarByDistrict?district_id={1}&date={0}", date, districtCode));
+            response.EnsureSuccessStatusCode();
+            string responseBody = await response.Content.ReadAsStringAsync();
+            return responseBody;
+        }
+    }
+    // Root myDeserializedClass = JsonConvert.DeserializeObject<Root>(myJsonResponse); 
+    public class Session
+    {
+        public string session_id { get; set; }
+        public string date { get; set; }
+        public int available_capacity { get; set; }
+        public int min_age_limit { get; set; }
+        public string vaccine { get; set; }
+        public List<string> slots { get; set; }
+    }
+
+    public class VaccineFee
+    {
+        public string vaccine { get; set; }
+        public string fee { get; set; }
+    }
+
+    public class Center
+    {
+        public int center_id { get; set; }
+        public string name { get; set; }
+        public string state_name { get; set; }
+        public string district_name { get; set; }
+        public string block_name { get; set; }
+        public int pincode { get; set; }
+        public int lat { get; set; }
+        public int @long { get; set; }
+        public string from { get; set; }
+        public string to { get; set; }
+        public string fee_type { get; set; }
+        public List<Session> sessions { get; set; }
+        public List<VaccineFee> vaccine_fees { get; set; }
+    }
+
+    public class Root
+    {
+        public List<Center> centers { get; set; }
+    }
+
+
+
+}
